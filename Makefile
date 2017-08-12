458420b1d306cab72160e0798cdfbfd83e10871f
Ben Tomasik <ben@tomasik.io>
2017-08-12 16:51:19 -0500

    Add docker container for building Lambda zip

--- Makefile
+++ Makefile
@@ -1,5 +1,8 @@
 AWS_PROFILE  := personal
 REGION       := us-east-1
+PWD          := $(shell pwd)
+
+BUILDER_IMAGE_TAG := photos3/python-builder
 
 
 .PHONY : stack
@@ -11,3 +14,12 @@ stack  :
 		stack.yml \
 		--diff \
 		-vvv
+
+.PHONY    : build.zip
+build.zip :
+	docker run \
+		-it \
+		--rm \
+		-v $(PWD):/code \
+		-w /code \
+		$(BUILDER_IMAGE_TAG)
---
+++ util/builder.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+set -e
+
+DEFAULT_UID=1000
+DEFAULT_GID=1000
+DEFAULT_ZIP=build.zip
+
+BUILD_UID=${BUILD_UID:-${DEFAULT_UID}}
+BUILD_GID=${BUILD_GID:-${DEFAULT_GID}}
+BUILD_ZIP=${BUILD_ZIP:-${DEFAULT_ZIP}}
+
+# Create virtualenv
+virtualenv /tmp/venv
+
+# Install package and all dependencies
+/tmp/venv/bin/pip install -e .
+
+# Build the zip
+cd /tmp/venv/lib/python3.6/site-packages/
+zip -r ${BUILD_ZIP} *
+chown ${BUILD_UID}:${BUILD_GID} ${BUILD_ZIP}
