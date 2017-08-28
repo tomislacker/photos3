562cd56ea0a0ccb6fb91a5edf77026220c8c7e2e
Ben Tomasik <ben@tomasik.io>
2017-08-28 15:07:00 -0500

    Add image processing skeleton

---
+++ photos3/imgprocess.py
@@ -0,0 +1,13 @@
+"""
+photos3.imgprocess
+==================
+Contains code for processing images
+"""
+def ingest_image(file_path):
+    """
+    Handles new image ingestion
+
+    :param file_path: Path to file to ingest
+    :type file_path: str
+    """
+    raise NotImplementedError
