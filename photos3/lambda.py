be69a822aa5c795fba5e3b36081b5a470936a4aa
Ben Tomasik <ben@tomasik.io>
2017-08-28 19:12:50 -0500

    Process images as time allows

--- photos3/lambda.py
+++ photos3/lambda.py
@@ -31,41 +31,51 @@ def process_new_image_queue(event, context):
     """
     print("Reading from {}".format(os.environ.get('TASK_QUEUE')))
     new_image_queue = sqs.get_queue_by_name(QueueName=os.environ.get('TASK_QUEUE'))
-    for msg_obj in new_image_queue.receive_messages():
-        # Decode S3 Event Notification received by SQS
-        s3_msg = json.loads(msg_obj.body)
 
-        # Iterate through each record of the S3 Event Notifications
-        # There's likely only one record per event, but we should still
-        # iterate through the list.
-        failed_objects = 0
+    has_messages = True
+    while has_messages:
+        # Assume there are no messages in the queue, letting the Lambda
+        # conclude, unless a message is received
+        has_messages = False
 
-        for record in s3_msg['Records']:
-            # Fetch the S3 bucket and object
-            s3_bucket = s3.Bucket(record['s3']['bucket']['name'])
-            s3_object = s3_bucket.Object(urllib.parse.unquote_plus(record['s3']['object']['key']))
-            s3_object_size = record['s3']['object']['size']
+        for msg_obj in new_image_queue.receive_messages(WaitTimeSeconds=5):
+            # Indicate that a message was received during this iteration
+            has_messages = True
 
-            # TODO Do not process objects over a certain size
+            # Decode S3 Event Notification received by SQS
+            s3_msg = json.loads(msg_obj.body)
 
-            print("Processing s3://{b}/{k} ({s} bytes)".format(
-                b=s3_object.bucket_name,
-                k=s3_object.key,
-                s=s3_object_size,
-            ))
+            # Iterate through each record of the S3 Event Notifications
+            # There's likely only one record per event, but we should still
+            # iterate through the list.
+            failed_objects = 0
 
-            # Ingest the image
-            try:
-                ingest_image(s3_object)
+            for record in s3_msg['Records']:
+                # Fetch the S3 bucket and object
+                s3_bucket = s3.Bucket(record['s3']['bucket']['name'])
+                s3_object = s3_bucket.Object(urllib.parse.unquote_plus(record['s3']['object']['key']))
+                s3_object_size = record['s3']['object']['size']
 
-            except Exception as e:
-                # Report the failure
-                failed_objects += 1
-                traceback.print_exception(*sys.exc_info())
+                # TODO Do not process objects over a certain size
 
-        if not failed_objects:
-            print("Removing queue message")
-            msg_obj.delete()
+                print("Processing s3://{b}/{k} ({s} bytes)".format(
+                    b=s3_object.bucket_name,
+                    k=s3_object.key,
+                    s=s3_object_size,
+                ))
+
+                # Ingest the image
+                try:
+                    ingest_image(s3_object)
+
+                except Exception as e:
+                    # Report the failure
+                    failed_objects += 1
+                    traceback.print_exception(*sys.exc_info())
+
+            if not failed_objects:
+                print("Removing queue message")
+                msg_obj.delete()
 
 if __name__ == '__main__':
     process_new_image_queue(None, None)
